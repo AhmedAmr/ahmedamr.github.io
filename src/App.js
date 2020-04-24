3ac783ce8fab800b1f7007f26d76ea8153fcf931
Ahmed Amr <ahmedamron@gmail.com>
2020-04-25 01:53:53 +0200

    Redirect source traffic to in-short page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import "bulma";
 import PageHeader from "./components/PageHeader";
 import PageFooter from "./components/PageFooter";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import InShortPage from "./pages/InShortPage";
 import EducationPage from "./pages/EducationPage";
 
@@ -14,8 +14,9 @@ function App() {
         <div class="hero is-dark is-fullheight is-bold">
           <PageHeader />
           <Switch>
-            <Route path="/in-short" component={InShortPage} exact />
+            <Route path="/in-short" component={InShortPage} exact/>
             <Route path="/education" component={EducationPage} exact />
+            <Redirect from='/' to='/in-short' exact></Redirect>
           </Switch>
           <PageFooter />
         </div>
